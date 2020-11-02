0536c0fafc98536fe67a23d324ca119a3aab0cd9
wpan11 <wpan11@illinois.edu>
2020-11-02 13:04:28 -0600

    Create MySQL database and tables with models.py Problem 1. Need to connect MySQL with Django
    Solution
    1. Use XAMPP to run MySQL at localhost:3306
    2. Migrate user table to MySQL with 'python manage.py migrate'
    3. Confirm table is created with command
    
    Note
    1. Need to create other tables and test with api in Django Rest Framework.

--- backend/api/models.py
+++ backend/api/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    userId = models.IntegerField(primary_key=True, null=False)
+    name = models.CharField('Name', max_length=100)
+    age = models.IntegerField('Age', null=False)
+
+    class Meta:
+        db_table = 'user'
