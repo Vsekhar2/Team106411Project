4bb5c8ba396416d02b81a1830d65f43924d2c64a
SvinSoftWare <vinodhsekhar@gmail.com>
2020-11-02 20:34:05 -0600

    Front end form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 import './App.css';
+import Form from "./Form";
 
 let dummydata = null;
 
@@ -28,6 +29,21 @@ class App extends Component {
 			})
   }
 
+
+  state = {
+
+    fields: {}
+
+  }
+
+  onSubmit = fields => {
+
+    this.setState({ fields });
+    console.log("App Component got: ", fields);
+
+
+  }
+
   render() {
     return(
       <div className="dummy">
@@ -35,7 +51,15 @@ class App extends Component {
         <div className='form-container'>
           {(this.state.show) ? dummydata : null}
         </div>
+        <Form onSubmit={fields => this.onSubmit(fields)} />
+        <p>
+        {JSON.stringify(this.state.fields, null, 2)}
+        </p>
       </div>
+
+
+
+
     );
   }
 }
