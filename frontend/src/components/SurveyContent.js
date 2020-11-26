bf8a8f5eabb920c2d94de1055b501fc75fa70a8c
blakeel2 <blakeel2@illinois.edu>
2020-11-26 17:10:02 -0600

    11/26 end of meeting save, blakeel2

--- frontend/src/components/SurveyContent.js
+++ frontend/src/components/SurveyContent.js
@@ -1,16 +1,48 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import User from "../User";
 import Experience from "../Experience";
 import Game from "../Game";
 
 let dummydata = null;
 
+class Survey extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      show: false
+    };
+    this.ClickHandler = this.ClickHandler.bind(this);
+  }
+
+  ClickHandler() {
+    console.log("Connecting to Django...");
+    axios.get("http://127.0.0.1:8000/api/")
+      .then((response) => {
+        console.log(response.data)
+        dummydata = JSON.stringify(response.data)
+        this.setState({
+          show: true
+        });
+      })
+      .catch((error) => {
+        console.log(error)
+      })
+  }
+  state = {
+    fields: {}
+  }
+  //Called on Submission of Form
+  onSubmit = fields => {
+    console.log("App Component got: ", fields);
+  }
+}
+
 const SurveyContent = () => {
   return (
     <div className="SurveyContent">
-      Survey
       <div className='form-container'>
-        {(this.state.show) ? dummydata : null}
+        {/*{(this.state.show) ? dummydata : null} */}
       </div>
       <User onSubmit={fields => this.onSubmit(fields)} />
       <br />
@@ -18,7 +50,7 @@ const SurveyContent = () => {
       <br />
       <Game onSubmit={fields => this.onSubmit(fields)} />
       <p>
-      {JSON.stringify(this.state.fields, null, 2)}
+      {/*{JSON.stringify(this.state.fields, null, 2)} */}
       </p>
     </div>
   )
